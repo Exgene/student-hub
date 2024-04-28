ce66882b4b90ba25a3251d58fb59bac84a66d200
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2024-04-28 21:45:46 +0530

    Update sitemap

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'http://localhost:3000' || 'https://student-hub-nitte.vercel.app/',
+  siteUrl: 'https://student-hub-nitte.vercel.app/',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
