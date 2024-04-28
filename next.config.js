5b301cfc57c4676b7c4ceda9159768e2bd11186d
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2024-04-28 22:47:27 +0530

    breaking change: update from legacy source to remotePattern

--- next.config.js
+++ next.config.js
@@ -7,7 +7,23 @@ await import('./src/env.js')
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    domains: ['nitte.edu.in'],
+    // domains: [
+    //   'nitte.edu.in,nmamit.nitte.edu.in,https://nmamit.nitte.edu.in',
+    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nitte.edu.in',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nmamit.nitte.edu.in',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
